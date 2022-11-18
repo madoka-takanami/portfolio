484b341eb4399f6051060361adc87ff2766dbe0d
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2022-11-18 22:57:38 +0900

    Add ログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :notion, presence: true
+  validates :introduction, length: { maximum: 250 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_18_034942) do
+ActiveRecord::Schema.define(version: 2022_11_18_082849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email", null: false
     t.string "generation"
     t.string "introduction", limit: 200
-    t.string "password", null: false
     t.string "avator"
     t.boolean "notion", default: true, null: false
+    t.string "name", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
