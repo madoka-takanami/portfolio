46fc8d07abc4a28ba773dec30f09f8814d868daa
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2022-11-18 17:10:42 +0900

    Add Userモデル作成、user新規登録画面作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#edb03a",
+          "secondary": "#68c5d6",
+          "accent": "#db484a",
+          "neutral": "#221924",
+          "base-100": "#FAF9FA",
+          "info": "#5FC5E7",
+          "success": "#0C6452",
+          "warning": "#C38513",
+          "error": "#E1362D",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
