4bab0772d65473076ea60f6c0372aad7cc82e02b
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2022-11-23 17:56:02 +0900

    Add 飲食店の詳細,編集,削除機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_21_232816) do
+ActiveRecord::Schema.define(version: 2022_11_23_082147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "restaurants", force: :cascade do |t|
-    t.integer "rst_type"
+    t.integer "rst_type", default: 0, null: false
     t.string "rst_name", null: false
     t.string "place"
     t.string "feature"
