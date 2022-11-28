7c8152f9cd6af4a44e676c0b470ace60399bdaf4
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2022-11-29 08:54:33 +0900

    FIx モーダル画面調整

--- app/javascript/packs/restaurant.js
+++ app/javascript/packs/restaurant.js
@@ -4,12 +4,12 @@ var closeBtn = document.getElementById('closeBtn');
 
 btn.addEventListener('click', function () {
   modal.style.display = 'block';
-})
+});
 
 ///ボタンを押した時、モーダルを消したい時
 closeBtn.addEventListener('click', function () {
   modal.style.display = 'none';
-})
+});
 
 //ボタンじゃなくて黒い部分を押した時、にモーダルを消したいとき
 //window.addEventListener('click', function (e) {
