21a69c156e68d939506a4765fc26f17a6f5c8d6a
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2022-12-05 21:55:39 +0900

    Add フォローボタンのajax処理

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -2,11 +2,10 @@ class VisitsController < ApplicationController
   def create
     @restaurants = Restaurant.find(params[:restaurant_id])
     current_user.visit(@restaurants)
-    redirect_to request.referer
   end
 
   def destroy
+    @restaurants = Restaurant.find(params[:restaurant_id])
     current_user.unvisited(params[:restaurant_id])
-    redirect_to request.referer
   end
 end
