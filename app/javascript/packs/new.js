cd49de3a192eb8449058193accc3f3980ce62aa3
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2022-12-12 16:53:12 +0900

    Fix spots/newで東京駅周辺の地図を表示

--- app/javascript/packs/new.js
+++ app/javascript/packs/new.js
@@ -1,39 +1,50 @@
-let map
-let marker
-
 function initMap() {
-  geocoder = new google.maps.Geocoder()
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 40.7828, lng: -73.9653 },
-    zoom: 12,
+  var target = document.getElementById('map');
+  var tokyo = { lat: 35.681167, lng: 139.767052 };  //東京駅の緯度経度
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: tokyo,
+    zoom: 16
   });
 
-  marker = new google.maps.Marker({
-    position: { lat: 40.7828, lng: -73.9653 },
-    map: map
-  });
+  //情報ウィンドウのインスタンスの生成（後でマーカーに紐付け）
+  var infowindow = new google.maps.InfoWindow();
 
+  //PlacesService のインスタンスの生成（引数に map を指定）
+  var service = new google.maps.places.PlacesService(map);
 
-  document.getElementById('search').addEventListener('click', function () {
+  //種類（タイプ）やキーワードをもとに施設を検索（プレイス検索）するメソッド nearbySearch()
+  service.nearbySearch({
+    location: tokyo,  //検索するロケーション
+    radius: 500,  //検索する半径（メートル）
+    type: ['store', 'cafe']  //タイプで検索。文字列またはその配列で指定
+    //キーワードで検索する場合は name:'レストラン' や ['レストラン','中華'] のように指定
+  }, callback);  //コールバック関数（callback）は別途定義
 
-    const inputKeyword = document.getElementById('keyword').value
-    const geocoder = new google.maps.Geocoder();
-
-    geocoder.geocode({ address: inputKeyword }, function (results, status) {
-      if (status == 'OK') {
-
-        map.setCenter(results[0].geometry.location);
-        marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-        });
-      } else {
-        alert('該当する結果はありませんでした:' + status)
+  //コールバック関数には results, status が渡されるので、status により条件分岐
+  function callback(results, status) {
+    // status は以下のような定数で判定（OK の場合は results が配列で返ってきます）
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      //results の数だけ for 文で繰り返し処理
+      for (var i = 0; i < results.length; i++) {
+        //createMarker() はマーカーを生成する関数（別途定義）
+        createMarker(results[i]);
       }
-    })
-  })
+    }
+  }
+
+  //マーカーを生成する関数（引数には検索結果の配列 results[i] が入ってきます）
+  function createMarker(place) {
+    //var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location  //results[i].geometry.location
+    });
+
+    //マーカーにイベントリスナを設定
+    marker.addListener('click', function () {
+      infowindow.setContent(place.name);  //results[i].name
+      infowindow.open(map, this);
+    });
+  }
 }
-
-
 window.initMap = initMap;
