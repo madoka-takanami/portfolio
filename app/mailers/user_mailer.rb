6b3531ed2f92b4d84e8d71def28944fc0039c029
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2022-12-17 11:33:40 +0900

    Add 飲食店ランダムメールの設定

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,15 @@
 class UserMailer < ApplicationMailer
 
-default from: 'from@example.com'
-
   def reset_password_email(user)
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
     mail(to: user.email, subject: "password has been reset")
+  end
+
+  def notion_email
+    @user = User.where(notion: true).shuffle.first
+    @restaurant = @user.restaurants.shuffle.first
+    @url = 'from@example.com'
+    mail(to: @user.email, subject: 'がらぽんごはん')
   end
 end
