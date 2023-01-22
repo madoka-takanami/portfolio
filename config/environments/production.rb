8fd2ff8d93daf3c5b3016eaa3968b1bedac1fa45
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2023-01-22 13:22:09 +0900

    user_name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,7 +103,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  :user_name      => "RepQ48SCQTivEe80MBabgA",
+  :user_name      => "apikey",
   :password       => ENV['SENDGRID_API_KEY'],
   :domain => 'herokuapp.com',
   :address => 'smtp.sendgrid.net',
