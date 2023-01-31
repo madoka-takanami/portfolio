fb90495d41dd32d419576c95168a03f8c4407b62
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2023-01-31 18:12:31 +0900

    Fix gstlogエラー

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -3,6 +3,8 @@ class GuestController < ApplicationController
 
   def create
     guest = User.find_by(email: ENV['GUEST_MAIL'])
+    puts "+++++++++++++++++++++++"
+    puts guest
     auto_login(guest)
     redirect_to restaurants_path
     flash[:success] = t('.login_as_guest')
