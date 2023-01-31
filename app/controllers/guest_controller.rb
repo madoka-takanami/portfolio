ef19b64aba3a04197e49043a3b1cc8e3dfaf26fa
madoka takanami <madokatakanami@madokanoMacBook-Air.local>
2023-01-31 18:29:11 +0900

    Fix gsterr

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -2,9 +2,7 @@ class GuestController < ApplicationController
   skip_before_action :require_login, only: %i[create]
 
   def create
-    guest = User.find_by(email: ENV['GUEST_MAIL'])
-    puts "+++++++++++++++++++++++"
-    puts guest
+    guest = User.find_by(email: "guest_gets@example.com")
     auto_login(guest)
     redirect_to restaurants_path
     flash[:success] = t('.login_as_guest')
